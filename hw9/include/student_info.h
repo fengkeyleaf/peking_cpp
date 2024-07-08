9f0c4721f6817649f4c15ee6514b84c7f790f76c
XIaoyu Tongyang <fengkeyleaf@gmail.com>
2024-07-08 09:35:10 -0400

    done with hw9

--- hw9/include/student_info.h
+++ hw9/include/student_info.h
@@ -10,6 +10,7 @@
  *     $1.0 basic operations on 7/7/2024
  */
 
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <map>
@@ -31,9 +32,43 @@ struct Student {
     int score;
 };
 
-
+// https://en.cppreference.com/w/cpp/utility/pair/make_pair
+// https://en.cppreference.com/w/cpp/container/multimap
+template<class Key, class T, class Compare = std::greater<Key>>
 class MyMultimap {
+    std::multimap<Key, T, Compare> M;
 
+public:
+    MyMultimap() { M.begin()->second; }
+
+    // https://www.geeksforgeeks.org/implementing-iterator-pattern-of-a-single-linked-list/
+    class iterator {
+        typename std::multimap<Key, T, Compare>::iterator iter;
+
+    public:
+        explicit iterator( typename std::multimap<Key, T, Compare>::iterator iter_ ) : iter( iter_ ) {}
+
+        bool operator!=( const iterator& iter_ );
+
+        typename std::multimap<Key, T, Compare>::iterator operator->();
+
+        iterator& operator++();
+
+        std::string operator*();
+    };
+
+    void insert( std::pair<Key, T> p );
+
+    void Set( Key k, T v );
+
+    void clear();
+
+    MyMultimap<Key, T, Compare>::iterator begin();
+
+    MyMultimap<Key, T, Compare>::iterator end();
+
+    // https://en.cppreference.com/w/cpp/container/multimap/find
+    MyMultimap<Key, T, Compare>::iterator find( const Key& k );
 };
 
 template<class T>
--- hw9/src/student_info.cpp
+++ hw9/src/student_info.cpp
@@ -19,6 +19,72 @@
  * @since 1.0
  */
 
+// --------------------------------------------------
+// class MyMultimap
+// --------------------------------------------------
+
+template<class Key, class T, class Compare>
+void MyMultimap<Key, T, Compare>::insert( std::pair<Key, T> p ) {
+    M.insert( p );
+}
+
+template<class Key, class T, class Compare>
+void MyMultimap<Key, T, Compare>::Set( Key k, T v ) {
+   auto r = M.equal_range( k );
+   for ( auto i = r.first; i != r.second; i++ ) {
+       i->second = v;
+   }
+}
+
+template<class Key, class T, class Compare>
+void MyMultimap<Key, T, Compare>::clear() {
+    M.clear();
+}
+
+template<class Key, class T, class Compare>
+typename MyMultimap<Key, T, Compare>::iterator MyMultimap<Key, T, Compare>::begin() {
+    return MyMultimap<Key, T, Compare>::iterator( M.begin() );
+}
+
+template<class Key, class T, class Compare>
+typename MyMultimap<Key, T, Compare>::iterator MyMultimap<Key, T, Compare>::end() {
+    return MyMultimap<Key, T, Compare>::iterator( M.end() );
+}
+
+template<class Key, class T, class Compare>
+typename MyMultimap<Key, T, Compare>::iterator MyMultimap<Key, T, Compare>::find( const Key &k ) {
+    return MyMultimap<Key, T, Compare>::iterator( M.find( k ) );
+}
+
+// --------------------------------------------------
+// class MyMultimap::iterator
+// --------------------------------------------------
+
+template<class Key, class T, class Compare>
+bool MyMultimap<Key, T, Compare>::iterator::operator!=( const MyMultimap::iterator &iter_ ) {
+    return iter != iter_.iter;
+}
+
+template<class Key, class T, class Compare>
+typename std::multimap<Key, T, Compare>::iterator MyMultimap<Key, T, Compare>::iterator::operator->() {
+    return iter;
+}
+
+template<class Key, class T, class Compare>
+typename MyMultimap<Key, T, Compare>::iterator& MyMultimap<Key, T, Compare>::iterator::operator++() {
+    ++iter;
+    return *this;
+}
+
+template<class Key, class T, class Compare>
+std::string MyMultimap<Key, T, Compare>::iterator::operator*() {
+    std::cout << "(" << iter->first << "," << iter->second << ")";
+    return std::string( "" );
+}
+
+// --------------------------------------------------
+// Helper function
+// --------------------------------------------------
 
 template<class T>
 void Print( T first, T last ) {
@@ -28,6 +94,12 @@ void Print( T first, T last ) {
 }
 
 void caller() {
+    // Debugging setting
+//    std::ifstream fi( "/home/sora/perking_cpp/hw9/in_stu_info.txt" );
+//    std::cin.rdbuf( fi.rdbuf() );
+//    std::ofstream fo = std::ofstream( "/home/sora/perking_cpp/hw9/out_stu_info_test.txt" );
+//    std::cout.rdbuf( fo.rdbuf() );
+
     Student s[] = { { "Tom",   80 },
                     { "Jack",  70 },
                     { "Jone",  90 },
@@ -36,7 +108,7 @@ void caller() {
 
     MyMultimap<std::string, int> mp;
     for ( int i = 0; i < 5; ++i )
-        mp.insert( make_pair( s[ i ].name, s[ i ].score ) );
+        mp.insert( std::make_pair( s[ i ].name, s[ i ].score ) );
     Print( mp.begin(), mp.end() ); //按姓名从大到小输出
 
     mp.Set( "Tom", 78 ); //把所有名为"Tom"的学生的成绩都设置为78
@@ -45,7 +117,7 @@ void caller() {
 
     MyMultimap<int, std::string, std::less<int> > mp2;
     for ( int i = 0; i < 5; ++i )
-        mp2.insert( make_pair( s[ i ].score, s[ i ].name ) );
+        mp2.insert( std::make_pair( s[ i ].score, s[ i ].name ) );
 
     Print( mp2.begin(), mp2.end() ); //按成绩从小到大输出
     mp2.Set( 70, "Error" );          //把所有成绩为70的学生，名字都改为"Error"
@@ -63,7 +135,7 @@ void caller() {
             if ( mp.find( name ) != mp.end() ) {
                 std::cout << "erroe" << std::endl;
             }
-            mp.insert( make_pair( name, score ) );
+            mp.insert( std::make_pair( name, score ) );
         } else if ( cmd == "Q" ) {
             std::cin >> name;
             MyMultimap<std::string, int>::iterator p = mp.find( name );
@@ -75,4 +147,3 @@ void caller() {
         }
     }
 }
-
